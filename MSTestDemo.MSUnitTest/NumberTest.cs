55b2c019c49f929b2bdd8832c3edb8208fe00ebf
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-07-29 23:40:48 +0530

    Unit Test demo using Microsoft Testing Framework MSTest

---
+++ MSTestDemo.MSUnitTest/NumberTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestDemo;
+
+namespace MSTestDemo.MSUnitTest
+{
+    [TestClass]
+    public class NumberTest
+    {
+        [TestMethod]
+        public void CalculateSum_SumOfIntegers_ReturnsTotal()
+        {
+            //Arrange
+            var number = new Number();
+
+            //Act
+            var result = number.CalculateSum(10, 20);
+
+            //Assert
+            Assert.AreEqual(result, 30);
+        }
+
+        [TestMethod]
+        public void CalculateSum_SumOfIntegers_ReturnsTotalNotEqual()
+        {
+            var number = new Number();
+
+            var result = number.CalculateSum(10, 20);
+
+            Assert.AreNotEqual(result, 20);
+        }
+    }
+}
---
+++ MSTestDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Number();
+
+            Console.WriteLine("Enter two integers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number.CalculateSum(a,b);
+
+            Console.WriteLine("Sum of the numbers is: " + sum);
+            Console.ReadLine();
+        }
+    }
+
+    public class Number
+    {
+        public int CalculateSum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
